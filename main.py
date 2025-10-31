eeb7b7b8e6f7bb15ef4cb95bbd859f04f6a3600f
marvellous <120428022+marviecephas@users.noreply.github.com>
2025-10-31 05:23:59 +0100

    Update main.py
    training( logistic_regression + random forest + xgboost ) + evaluation (classification_report)

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from google.colab import drive
+ from google.colab import drive
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 drive.mount('/content/drive')
 file_path = '/content/drive/MyDrive/order.csv'
@@ -16,6 +20,9 @@ categorical_df = df.select_dtypes(include = 'object')
 top_15_items = df['Items in order'].value_counts().nlargest(15).index
 df_top_15 = df[df['Items in order'].isin(top_15_items)]
 
+
+
+#Data visualization (remove triple quotes and run. Note : it might take a while to run)
 '''for col in numerical_df.columns :
   plt.figure(figsize = (10, 8))
   sns.histplot(data = df, x = col, kde = True)
@@ -95,7 +102,55 @@ categorical_features = [
 
 df = pd.get_dummies(df, columns = categorical_features , drop_first = True)
 
+# Clean column names to remove problematic characters for XGBoost
+df.columns = df.columns.str.replace('[^A-Za-z0-9_]+', '_', regex=True)
+
 
 df.dropna(inplace = True)
 
-print(df.head(50))
+df['Items_in_order'] = df['Items_in_order'].apply(lambda x : x if x in top_15_items else 'Other')
+
+df_main_items = df[df['Items_in_order'] != 'Other']
+df_others = df[df['Items_in_order'] == 'Other']
+n_samples = df_main_items.value_counts().max()
+df_other_sampled = df_others.sample(n = n_samples, random_state = 42)
+
+df = pd.concat([df_main_items, df_other_sampled])
+
+X = df.drop('Items_in_order', axis = 1)
+y_raw = df['Items_in_order']
+
+le = LabelEncoder()
+y = le.fit_transform(y_raw)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+logr = LogisticRegression()
+logr.fit(X_train, y_train)
+y_pred = logr.predict(X_test)
+y_pred_label = le.inverse_transform(y_pred)
+y_test_label = le.inverse_transform(y_test)
+
+rf = RandomForestClassifier(n_estimators = 100, random_state = 42, n_jobs = -1)
+rf.fit(X_train, y_train)
+y_pred_rf = rf.predict(X_test)
+y_pred_rf_label = le.inverse_transform(y_pred_rf)
+
+xg = XGBClassifier(n_estimators = 100, random_state = 42, use_label_encoder = False, eval_metric = 'mlogloss')
+xg.fit(X_train, y_train)
+y_pred_xg = xg.predict(X_test)
+y_pred_xg_label = le.inverse_transform(y_pred_xg)
+
+class_rpt_rf = classification_report(y_test_label, y_pred_label)
+
+
+print(f'Logisric Regression Report {class_rpt_rf}')
+
+
+class_rpt_rf = classification_report(y_test_label, y_pred_rf_label)
+
+print(f'Random Forest Report {class_rpt_rf}')
+
+
+
+class_rpt_xg = classification_report(y_test_label, y_pred_xg_label)
+
+print(f'XGBoost Report {class_rpt_xg}')
